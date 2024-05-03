0733dcd933b7239f7482d676bd024c6591879205
Sijal Manandhar <sijal.m06@gmail.com>
2024-05-03 14:52:48 +0545

    gh-pages setup

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'v2',
   plugins: [react()],
   server: {
     http: 'http://localhost',
     port: 3000,
-    host: true,
   },
 });
